88ce281597d3d4779424860d91837e64c95ff45f
Maya <21083010018@student.upnjatim.ac.id>
2022-09-10 17:22:29 +0700

    Tugas1.pdf

---
+++ Week2/Tugas1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "-----------------------------------"
+echo "Tugas 1 Sisop Kelas A"
+echo "-----------------------------------"
+echo "Nama: Meisya Vira Amelia"
+echo "NPM : 21083010018"
+echo "-----------------------------------"
+echo "notes:"
+echo "-----------------------------------"
+echo "Halo dunia!"
+echo "Ini adalah skrip Bash pertamaku di linux"
